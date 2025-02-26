c4c3d5f636dac3f283a412fddbad5255fe898da1
studentskeblokade <studentskeblokade@gmai.com>
2025-02-26 23:50:53 +0100

    Malo refactor

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,12 +2,9 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const vesti = defineCollection({
-	// Load Markdown and MDX files in the `src/content/vesti/` directory.
 	loader: glob({ base: './src/content/vesti', pattern: '**/*.md' }),
-	// Type-check frontmatter using a schema
 	schema: ({ image }) => z.object({
 		title: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: image()
@@ -15,21 +12,16 @@ const vesti = defineCollection({
 });
 
 const akcije = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/akcije', pattern: '**/*.md' }),
-	// Type-check frontmatter using a schema
 	schema: ({ image }) => z.object({
 		title: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		heroImage: image()
 	}),
 });
 
 const linkovi = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/linkovi', pattern: '*.md' }),
-	// Type-check frontmatter using a schema
 	schema: () => z.object({
 		link: z.string(),
 		linkovi: z.object({
@@ -41,16 +33,12 @@ const linkovi = defineCollection({
 });
 
 const pocetna = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/stranice', pattern: '**/početna.md' }),
-	// Type-check frontmatter using a schema
 	schema: () => z.object({}),
 });
 
 const zahtevi = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/stranice', pattern: '**/zahtevi.md' }),
-	// Type-check frontmatter using a schema
 	schema: () => z.object({
 		naslov: z.string(),
 		zahtevi: z.object({
@@ -61,16 +49,12 @@ const zahtevi = defineCollection({
 });
 
 const oblokadama = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/stranice', pattern: '**/o-blokadama.md' }),
-	// Type-check frontmatter using a schema
 	schema: () => z.object({}),
 });
 
 const zaglavlje = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/stranice', pattern: '**/zaglavlje.md' }),
-	// Type-check frontmatter using a schema
 	schema: () => z.object({
 		naslov: z.string(),
 		linkovi: z.object({
@@ -81,9 +65,7 @@ const zaglavlje = defineCollection({
 });
 
 const ostalo = defineCollection({
-	// Load Markdown and MDX files in the `src/content/akcije/` directory.
 	loader: glob({ base: './src/content/stranice', pattern: '**/ostalo.md' }),
-	// Type-check frontmatter using a schema
 	schema: () => z.object({
 		naslov: z.string(),
 		akcije: z.string(),
