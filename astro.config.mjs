3794809e544f440626e0526f3204bbf455c3748d
Veliki Humus <humuseater@proton.me>
2025-03-09 19:22:20 +0000

    Fix contact page and other contact links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bighumus.github.io/bioloski-sajt/',
+	site: 'https://bioloski.blokade.org',
 	integrations: [sitemap(), icon()],
 	redirects: {
 		'/sub': '/sr-lat/linkovi/studenti_u_blokadi',
