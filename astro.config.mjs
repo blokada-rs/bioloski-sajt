2c1e4ac41e5010ef8507d09bbcde8000e2b3e7b0
Veliki Humus <humuseater@proton.me>
2025-03-09 16:46:50 +0000

    Modify the site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blokade.org',
+	site: 'https://bioloski.blokade.org',
 	integrations: [sitemap(), icon()],
 	redirects: {
 		'/sub': '/sr-lat/linkovi/studenti_u_blokadi',
